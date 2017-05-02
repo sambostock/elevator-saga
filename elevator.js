f297c72954e01e5e3129b468a2b15a56912106d2
Sam Bostock <bostocksamuel@gmail.com>
2017-05-02 18:42:46 -0400

    Use all elevators

--- elevator.js
+++ elevator.js
@@ -1,18 +1,18 @@
 {
   init: function(elevators, floors) {
-    const elevator = elevators[0];
+    elevators.forEach(elevator => {
+      elevator.on("idle", function() {
+        const pressedFloors = elevator.getPressedFloors();
 
-    elevator.on("idle", function() {
-      const pressedFloors = elevator.getPressedFloors();
-
-      if (pressedFloors.length > 0) {
-        const nextFloor = pressedFloors[0];
-        elevator.goToFloor(nextFloor);
-      } else {
-        floors.forEach(function(floor, index) {
-          elevator.goToFloor(index);
-        });
-      }
+        if (pressedFloors.length > 0) {
+          const nextFloor = pressedFloors[0];
+          elevator.goToFloor(nextFloor);
+        } else {
+          floors.forEach(function(floor, index) {
+            elevator.goToFloor(index);
+          });
+        }
+      });
     });
   },
 
