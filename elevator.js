9aad2282b9dc13b365d8951894cbf94638049e3c
Sam Bostock <bostocksamuel@gmail.com>
2017-05-02 18:39:01 -0400

    Prioritize dropping off passengers when idle

--- elevator.js
+++ elevator.js
@@ -3,9 +3,16 @@
     const elevator = elevators[0];
 
     elevator.on("idle", function() {
-      floors.forEach(function(floor, index) {
-        elevator.goToFloor(index);
-      });
+      const pressedFloors = elevator.getPressedFloors();
+
+      if (pressedFloors.length > 0) {
+        const nextFloor = pressedFloors[0];
+        elevator.goToFloor(nextFloor);
+      } else {
+        floors.forEach(function(floor, index) {
+          elevator.goToFloor(index);
+        });
+      }
     });
   },
 
